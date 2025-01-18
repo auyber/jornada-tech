c0e15311bbf46372cdfed56c72a235eb8dcb8a8f
auyber <160794306+auyber@users.noreply.github.com>
2025-01-18 10:32:35 -0300

    feat: create ex 6-arrays-metodos

---
+++ praticando/javascript/exercicios/6-arrays-metodos.js
@@ -0,0 +1,294 @@
+/*
+//TOTAL = 
+//PUSH()
+//FÁCIL
+//1. Exercício 1
+let frutas = ["maçã", "banana", "laranja"];
+
+frutas.push("manga");
+
+console.log(frutas);
+
+//2. Exercício 2
+let numeros = [1, 2, 3, 4];
+
+numeros.push(5);
+
+console.log(numeros);
+
+//MÉDIO
+//3. Exercício 3
+let alunos = ["Ana", "Carlos", "Beatriz"];
+
+alunos.push("Auyber","Mauro");
+
+console.log(alunos);
+
+//4. Exercício 4
+let times = ["Flamengo", "São Paulo", "Palmeiras"];
+
+times.push("Barça","Lamaçal","BebunsFC");
+
+console.log(times);
+
+//DIFÍCIL
+//5. Exercício 5
+let produtos = [
+    {
+        nome: "Camiseta", 
+        preco: 29.90
+    }, 
+    {
+        nome: "Calça", 
+        preco: 79.90
+    }
+];
+
+produtos.push({nome: "Regata", preco: 15.00});
+
+console.log(produtos);
+
+//6. Exercício 6
+let ids = [101, 102, 103, 104];
+let novosIds = [105, 106];
+
+ids.push(...novosIds); // se utilizar só (novosIds) ele envia o array inteiro "[105, 106]". para add os valores use o espalhamento "..."
+
+console.log(ids);
+
+//POP()
+//FÁCIL
+//1. Exercícios
+let frutas = ["maçã", "banana", "laranja"];
+
+frutas.pop();
+
+console.log(frutas);
+
+//2. Exercício 2
+let numeros = [10, 20, 30, 40];
+
+numeros.pop();
+
+console.log(numeros);
+
+//MÉDIO
+//3. Exercício 3
+let cores = ["vermelho", "azul", "verde", "amarelo"];
+
+let remover = cores.pop();
+
+console.log(remover);
+console.log(cores);
+
+//4. Exercício 4
+let animais = ["gato", "cachorro", "pássaro", "hamster"];
+
+animais.pop();
+
+console.log(animais);
+
+//DIFÍCIL
+//5. Exercício 5
+let pilha = [1, 2, 3, 4, 5];
+
+for(i = 0; i < 2; i++){ //utilizei o loop para utilizar o pop 2vzs
+    pilha.pop();
+}
+
+//pilha.pop(); //outra opção é chamar o pop 2 vezes
+//pilha.pop();
+
+console.log(pilha);
+
+//6. Exercício 6
+let documentos = [
+    {
+        nome: "contrato.pdf"
+    }, 
+    {
+        nome: "relatorio.docx"
+    }, 
+    {
+        nome: "imagem.jpg"
+    }
+];
+
+let retirado = documentos.pop();
+
+console.log(retirado);
+
+//UNSHIFT()
+//FÁCIL
+//1. Exercício 1
+let frutas = ["banana", "maçã", "laranja"];
+
+frutas.unshift("manga");
+
+console.log(frutas);
+
+//2 Exercício 2
+let numeros = [2, 3, 4];
+
+numeros.unshift(1);
+
+console.log(numeros);
+
+//MÉDIO
+//3. Exercício 3
+let carros = ["Fusca", "Gol", "Civic"];
+
+carros.unshift("Lancer Evo", "Subaru Imprenza");
+
+console.log(carros);
+
+//4. Exercício 4
+let paises = ["Brasil", "Argentina", "Chile"];
+
+paises.unshift("Peru", "Colômbia");
+
+console.log(paises);
+
+//DIFÍCIL
+//5. Exercício 5
+let pessoas = [
+    {
+        nome: "João",
+        idade: 30
+    }, 
+    {
+        nome: "Maria",
+        idade: 25
+    }
+];
+
+pessoas.unshift({nome: "José", idade: 40});
+
+console.log(pessoas);
+
+//6. Exercício 6
+let livros = ["O Hobbit", "1984", "Dom Quixote"];
+
+livros.unshift("Moby Dick", "A Guerra dos Tronos");
+
+console.log(livros);
+
+//SHIFT()
+//FÁCIL
+//1. Exercício 1
+let frutas = ["maçã", "banana", "laranja"];
+
+frutas.shift();
+
+console.log(frutas);
+
+//2. Exercício 2
+let numeros = [10, 20, 30, 40];
+
+numeros.shift();
+
+console.log(numeros);
+
+//MÉDIO
+//3. Exercício 3
+let cores = ["vermelho", "azul", "verde", "amarelo"];
+
+let remover = console.log(cores.shift());
+
+console.log(cores);
+
+//4. Exercício 4
+let alunos = ["Ana", "Carlos", "Beatriz", "Davi"];
+
+let remover = console.log(alunos.shift());
+
+console.log(alunos);
+
+//DIFÍCIL
+//5. Exercício 5
+let notas = [8.5, 7.2, 9.0, 6.8];
+
+notas.shift()
+
+function media(notas){
+    let resultado = 0
+    for(let i = 0; i <= notas.length - 1; i++){
+        resultado += notas[i]; //acessa o elemento na posição i do array notas
+    }
+    resultado = resultado / notas.length;
+    console.log(resultado);
+};
+
+media(notas);
+
+//6. Exercício 6
+let cidades = ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Porto Alegre"];
+
+cidades.shift();
+
+//utilizando o for
+for (let i = 0; i < cidades.length; i++) { // Loop 'for' para iterar sobre o array.
+    console.log(i + ": " + cidades[i]); // Exibe o índice e o nome da cidade.
+}
+
+//utilizando o indexOf
+cidades.forEach(function(nome, indice){
+    console.log(indice + ": " + nome);
+});
+
+console.log(cidades);
+
+//INDEXOF()
+//FÁCIL
+//1. Exercício 1
+let frutas = ["maçã", "banana", "laranja"];
+
+console.log(frutas.indexOf("banana"));
+
+//2. Exercício 2
+let numeros = [10, 20, 30, 40];
+
+console.log(numeros.indexOf(30));
+
+//MÉDIO
+//3. Exercício 3
+let animais = ["gato", "cachorro", "pássaro", "hamster"];
+
+console.log(animais.indexOf("pássaro"));
+
+//4. Exercício 4
+let cores = ["azul", "verde", "amarelo", "vermelho"];
+
+console.log(cores.indexOf("vermelho"));
+
+//DIFÍCIL
+//5. Exercício 5
+let pessoas = ["João", "Maria", "Pedro", "Ana"];
+
+console.log(pessoas.indexOf("Maria"));
+
+pessoas[1] = "Paula";
+
+console.log(pessoas);
+*/
+//6. Exercício 6
+let produtos = [
+    {nome: "Camiseta", preco: 29.90},
+    {nome: "Calça", preco: 79.90}
+];
+
+// Encontrar o índice do produto "Calça"
+let indice = produtos.findIndex(function(produto) {
+    return produto.nome === "Calça";
+});
+
+if (indice !== -1) {
+    console.log("Índice: " + indice); // Saída: 1
+    console.log("Produto: ", produtos[indice]); // Saída: {nome: "Calça", preco: 79.90}
+} else {
+    console.log("Produto não encontrado.");
+}
+
+
+
+
