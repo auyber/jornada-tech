0b09c87a1f73cf2fae887e4848ba2e6f26fc0436
auyber <160794306+auyber@users.noreply.github.com>
2025-01-22 14:09:08 -0300

    feat: create ex 12-objects

---
+++ praticando/javascript/exercicios/12-objetos.js
@@ -0,0 +1,102 @@
+//Estou focando apenas nos primeiros exercicios de cada tópico, para continuar meus estudos.
+//vou resolvendo mais exercicios todos os dias
+
+/*
+//TOTAL =
+
+//CRIAÇÃO E MANIPULAÇÃO DE OBJETOS
+//1.
+let carro = {
+    marca: "Subaru",
+    modelo: "Imprenza",
+    ano: 2025
+}
+
+console.log(carro)
+
+//2.
+let carro = {
+    marca: "Subaru",
+    modelo: "Imprenza",
+    ano: 2025
+}
+
+carro.cor = "Azul";
+
+console.log(carro)
+
+//FALTA EX: 3., 4., 5. e 6.
+
+//NOTAÇÃO LITERAL E CONSTRUTOR
+//1. notação literal
+let otoCarro = {
+    marca: "Mitsubishi",
+    modelo: "Lancer Evo X",
+    ano: 2025,
+    cor: "Azul Escuro"
+}
+
+console.log(otoCarro)
+
+//2.construtor object()
+let carro = new Object();
+
+carro.marca = "Lamborguini";
+carro.modelo = "Diablo SV";
+carro.ano = 2010;
+carro.cor = "amarelo";
+
+console.log(carro)
+
+
+//FALTA EX 3., 4., 5. e 6.
+
+//NOTAÇÃO PONTO E COLCHETES
+//1. pontos
+let otoCarro = {
+    marca: "Mitsubishi",
+    modelo: "Lancer Evo X",
+    ano: 2025,
+    cor: "Azul Escuro"
+}
+
+console.log(otoCarro.modelo)
+
+//2. colchetes
+let carro = new Object();
+
+carro.marca = "Lamborguini";
+carro.modelo = "Diablo SV";
+carro.ano = 2010;
+carro.cor = "amarelo";
+
+carro["cor"] = "vermelho";
+
+console.log(carro["cor"])
+
+//FALTA EX 3., 4., 5. e 6.
+
+//MANIPULAÇÃO DE OBJETOS
+//1. 
+livro = {
+    titulo: "Harry Potter",
+    autor: "JK Alguma Coisa"
+}
+
+livro.ano = 2000;
+
+console.log(livro)
+
+//2. 
+let otoCarro = {
+    marca: "Mitsubishi",
+    modelo: "Lancer Evo X",
+    ano: 2025,
+}
+
+otoCarro.modelo = "Evo XI";
+
+console.log(otoCarro)
+*/
+
+//FALTA EX: 3., 4., 5. e 6.
