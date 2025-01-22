8e0a01fd49d1b6fbcc5ed9e553a3c17a7d34d606
auyber <160794306+auyber@users.noreply.github.com>
2025-01-22 14:43:02 -0300

    feat: create ex 13-new-this-keys

---
+++ praticando/javascript/exercicios/13-new-this-keys.js
@@ -0,0 +1,75 @@
+//Estou focando apenas nos primeiros exercicios de cada tópico, para continuar meus estudos.
+//vou resolvendo mais exercicios todos os dias
+
+/*
+//TOTAL =
+
+//Exercícios de Objetos, new, this, keys, values, entries, herança e prototipagem
+
+//FOCADO EM NEW
+//1.
+function Carro(marca, modelo){
+this.marca = marca;
+this.modelo = modelo;
+}
+
+const meuCarro = new Carro("Ford", "Focus");
+console.log(meuCarro.marca)
+console.log(meuCarro.modelo)
+
+
+//FALTA O 2. e 3.
+
+//FOCADO EM THIS
+//1.this em função simples
+
+let pessoa = {
+    nome: "Auyber",
+    idade: 33,
+    saudacao: function(){
+        console.log("Olá, me chamo "+ this.nome + " e tenho " + this.idade)
+    }
+}
+
+pessoa.saudacao()
+
+// FALTA EX 2. e 3.
+
+//FOCADO EM OBJECT.KEYS
+//1. object.keys
+let pessoa = {
+    nome: "Auyber",
+    idade: "33",
+    profissão: "Desenvolvedor"
+};
+
+console.log(Object.keys(pessoa))
+
+//FALTA EX 2. e 3.
+
+//FOCADO EM OBJECT.VALUES
+//1. Object.values()
+let livro = {
+    titulo: "Silmarilion",
+    autor: "J.R.R. Tolkien",
+    ano: 1977,
+}
+
+console.log(Object.values(livro))
+
+//FALTA EX 2. e 3.
+
+//FOCADOS EM OBJECT.ENTRIES
+//1. pares chave-valor
+let aluno = {
+    nome: "Auyber",
+    idade: 33,
+    nota: 10,
+}
+
+console.log(Object.entries(aluno))
+*/
+//FALTA EX 2. e 3.
+
+//PROTOTIPAGEM E HERANÇA
+//1.herança simples
