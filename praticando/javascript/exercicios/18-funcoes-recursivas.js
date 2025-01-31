cb3fcdeb22830d2cb670f7f6116747c7c2874f11
auyber <160794306+auyber@users.noreply.github.com>
2025-01-31 13:52:30 -0300

    feat: create ex18 funcoes-recursivas

---
+++ praticando/javascript/exercicios/18-funcoes-recursivas.js
@@ -0,0 +1,8 @@
+//Estou focando apenas nos primeiros exercicios de cada tópico, para continuar meus estudos.
+//vou resolvendo mais exercicios todos os dias
+
+/**/
+//TOTAL =
+
+//Exercícios de Funções Recursivas
+//1. soma de números
