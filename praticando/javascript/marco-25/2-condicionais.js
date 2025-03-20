23b62af15817f2b763f649619a22e09190694953
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 15:33:15 -0300

    feat: create 2-condicionais

---
+++ praticando/javascript/marco-25/2-condicionais.js
@@ -0,0 +1,62 @@
+//1.Crie uma função que recebe dois números e retorna o maior deles.
+/*
+function maior(n1, n2) {
+    if (n1 > n2) {
+        return n1;
+    } else {
+        return n2
+    }
+}
+
+console.log(maior(4, 9))
+
+
+//2. Crie uma função que verifica se um número é par ou ímpar. A função deve retornar "par" ou "ímpar".
+function evenOdd(n1) {
+    if (n1 % 2 === 0) {
+        return `O número ${n1} é par`
+    } else {
+        return `O número ${n1} é impar`
+    }
+}
+
+console.log(evenOdd(7))
+
+
+//3. Crie uma função que recebe a idade de uma pessoa e retorna se ela é maior de idade ("maior de idade") ou menor de idade ("menor de idade").
+function isAdult(age) {
+    if (age >= 18) {
+        return "Você é maior de idade"
+    } else {
+        return "Você é menor de idade"
+    }
+}
+
+console.log(isAdult(33))
+
+
+//4. Crie uma função que recebe um número e verifica se ele é positivo, negativo ou igual a zero. A função deve retornar uma string com a descrição.
+function posNegZero(n) {
+    if (n > 0) {
+        return `O número ${n} é positivo`
+    } else if (n < 0) {
+        return `O número ${n} é negativo`
+    } else {
+        return `O número é zero`
+    }
+}
+
+console.log(posNegZero(-100000))
+
+
+//5.Crie uma função que recebe um número e verifica se ele é múltiplo de 3. Retorne "é múltiplo de 3" ou "não é múltiplo de 3
+function multiple(n) {
+    if (n % 3 === 0) {
+        return `O número ${n} é múltiplo de 3`
+    } else {
+        return `O número ${n} não é múltiplo de 3`
+    }
+}
+
+console.log(multiple(11))
+*/
