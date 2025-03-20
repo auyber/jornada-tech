2f6a37ada5fc156dbc55e2743c712cce913bed21
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 14:28:40 -0300

    feat: update praticando and create marco-25

---
+++ praticando/javascript/marco-25/1-operadores-logicos.js
@@ -0,0 +1,51 @@
+//1. Crie um programa que verifique se uma pessoa é maior de idade. A pessoa é maior de idade se tiver 18 anos ou mais.
+/*
+let age = 17
+    if (age >= 18) {
+        console.log("Você é maior de idade!")
+    } else {
+        console.log("Você é um aborrescente!")
+    }
+
+console.log()
+
+
+//2.Escreva uma função que checa se um número fornecido é par, retornando true se for par e false se for ímpar.
+let number = 22
+if (number % 2 == 0) {
+    console.log("O numero é par")
+} else {
+    console.log("O numero é impar")
+}
+
+console.log()
+
+
+//3.Crie uma função que retorna true se um número for positivo ou zero e false caso contrário.
+function positive(number) {
+    return number >= 0 ? "true" : "false";
+}
+
+console.log(positive(-10))
+
+
+//4.Escreva uma função que verifica se dois valores são iguais. A função deve retornar true se forem iguais e false caso contrário.
+
+function equals (number1, number2) {
+    return number1 === number2;
+}
+
+console.log(equals(1, 2))
+
+//5.Crie uma função que verifica se um eleitor pode votar. A pessoa pode votar se tiver 16 anos ou mais.
+function canVote(age) {
+    if (age >= 16) {
+        console.log("Você pode votar")
+    } else {
+        console.log("Você não pode votar")
+    }
+}
+
+console.log(canVote(16))
+*/
+
