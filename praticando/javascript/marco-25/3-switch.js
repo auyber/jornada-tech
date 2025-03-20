8a7345d4e65ee784ac908ed76161614e62bbfc87
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 15:50:39 -0300

    feat: create 3-switch

---
+++ praticando/javascript/marco-25/3-switch.js
@@ -0,0 +1,47 @@
+//1. Crie uma função que recebe um número de 1 a 7 e retorna o nome do dia da semana correspondente.
+/*
+function day(n) {
+    switch (n) {
+        case 1:
+            return "Domingo"
+        case 2:
+            return "Segunda-feira"
+        case 3:
+            return "Terça-feira"
+        case 4:
+            return "Quarta-feira"
+        case 5:
+            return "Quinta-feira"
+        case 6:
+            return "Sexta-feira"
+        case 7:
+            return "Sábado"
+        default:
+            return "Número inválido, insira um valor de 1 a 7"
+    }
+}
+
+console.log(day(12))
+
+
+//2.Crie uma função que recebe uma nota de 0 a 10 e retorna a classificação: 9 e 10: A; 7 a 8: B; 5 a 6: C; Abaixo de 5: D
+
+function grade(n) {
+    switch(true) {
+        case (n > 10):
+            return "Como você fez a proeza de tirar mais que 10?"
+        case (n > 8):
+            return "A"
+        case (n > 6):
+            return "B"
+        case (n > 4):
+            return "C"
+        case (n >= 0):
+            return "D"
+        default:
+            return "Você é péssimo, conseguiu tirar menos que 0"
+    }
+}
+
+console.log(grade(11))
+*/
