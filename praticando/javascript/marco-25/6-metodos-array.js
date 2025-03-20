9069653486e1b585d655b3f31ac066e07561a37e
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 17:17:49 -0300

    feat: create 6-metodos-array

---
+++ praticando/javascript/marco-25/6-metodos-array.js
@@ -0,0 +1,81 @@
+//PUSH
+//1. Dado o array let frutas = ["maçã", "banana", "laranja"];, adicione o elemento "manga" ao final do array utilizando o método push(). Imprima o array após a modificação
+/*
+let frutas = ["maçã", "banana", "laranja"]
+
+frutas.push("manga")
+
+console.log(frutas)
+
+
+//2. Crie um array let numeros = [1, 2, 3, 4]; e use o push() para adicionar o número 5 ao final. Mostre o array após a alteração.
+
+let numeros = [1, 2, 3, 4]
+
+numeros.push(5)
+
+console.log(numeros)
+*/
+
+
+
+
+
+
+
+
+
+//POP
+//1. Dado o array let frutas = ["maçã", "banana", "laranja"];, use o método pop() para remover o último item do array e mostre o array após a remoção.
+/*
+let frutas = ["maçã", "banana", "laranja"]
+
+frutas.pop(2)
+
+console.log(frutas)
+
+
+//2. Crie um array let numeros = [10, 20, 30, 40]; e remova o último número com o método pop(). Imprima o array após a operação.
+
+let numeros = [10, 20, 30, 40]
+
+numeros.pop(numeros[numeros.length - 1])
+
+console.log(numeros)
+*/
+
+
+
+
+
+
+
+
+//UNSHIFT
+//1. Dado o array let frutas = ["banana", "maçã", "laranja"];, adicione o elemento "manga" no início do array utilizando o método unshift(). Mostre o array resultante.
+/*
+let frutas = ["banana", "maçã", "laranja"]
+
+frutas.unshift("manga")
+
+console.log(frutas)
+
+
+//2. Crie um array let numeros = [2, 3, 4]; e adicione o número 1 no início usando o método unshift(). Imprima o array depois da alteração.
+
+let numeros = [2, 3, 4]
+
+numeros.unshift(1)
+
+console.log(numeros)
+*/
+
+
+
+
+
+
+
+
+//SHIFT
+//1.Dado o array let frutas = ["maçã", "banana", "laranja"];, remova o primeiro elemento do array usando o método shift(). Mostre o array após a remoção.
