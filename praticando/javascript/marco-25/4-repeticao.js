36e196278662fec9296ba50b0a61e7b12a828359
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 16:23:39 -0300

    feat: create repeticao

---
+++ praticando/javascript/marco-25/4-repeticao.js
@@ -0,0 +1,37 @@
+//1. Usando for: Crie um programa que receba um número N do usuário e calcule a soma de todos os números inteiros de 1 até N. O resultado da soma deve ser impresso na tela. Por exemplo, se N for 5, a soma será 1 + 2 + 3 + 4 + 5.
+/*
+function some(n) {
+    let result = 0
+    for (let i =0; i <= n; i++) {
+        result += i
+    }
+    return result
+}
+
+console.log(some(10))
+
+
+//2. Usando for: Crie um programa que faça uma contagem de 1 até um número N fornecido pelo usuário. O programa deve imprimir cada número da contagem em uma linha separada. Por exemplo, se N for 4, a saída será: 1 2 3 4
+function counter(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+
+console.log(counter(10))
+
+
+//3.
+
+
+
+//4.Usando While) Crie um programa que faça uma contagem regressiva a partir de um número N fornecido pelo usuário, até o número zero. Cada número deve ser impresso na tela, um por linha. Por exemplo, se N for 5, a saída será: 5 4 3 2 1 0
+function regressive(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
+
+console.log(regressive(10))
+*/
