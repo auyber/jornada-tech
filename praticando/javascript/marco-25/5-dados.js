5bd136c9e0a6653d22d20bd0c8e43e8d4b83b0bc
auyber <160794306+auyber@users.noreply.github.com>
2025-03-20 17:07:56 -0300

    feat: create 5-dados

---
+++ praticando/javascript/marco-25/5-dados.js
@@ -0,0 +1,17 @@
+//1. Crie um array com 5 números inteiros e imprima cada um deles no console. A seguir, acesse e imprima o primeiro e o último elemento do array.
+/*
+let array = [1, 2, 3, 4 ,5]
+
+console.log(array)
+console.log(array[0])
+console.log(array[array.length - 1]) //busca o valor no array, em relação ao índice. o tamanho do array é 5, porém o último índice é 4 (começa em 0)
+
+
+//2.Dado um array de números, crie uma função que retorne a soma de todos os elementos do array.
+
+let array = [1, 2, 3, 4, 5, 6]
+
+let some = array.reduce((acc, n) => acc + n, 0)
+
+console.log(some)
+*/
