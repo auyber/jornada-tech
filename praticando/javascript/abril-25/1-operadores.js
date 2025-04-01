b9f93f11a3ce86275cfdc433bb39f359e57d35ee
auyber <160794306+auyber@users.noreply.github.com>
2025-04-01 09:55:50 -0300

    feat: create 1 and 2

---
+++ praticando/javascript/abril-25/1-operadores.js
@@ -0,0 +1,17 @@
+//1. Crie um programa que verifique se uma pessoa é maior de idade. A pessoa é maior de idade se tiver 18 anos ou mais.
+/*
+function greater(age) {
+    return age >= 18
+}
+
+console.log(greater(18))
+
+
+//2. Escreva uma função que checa se um número fornecido é par, retornando true se for par e false se for ímpar.
+function even (n) {
+    return n % 2 === 0
+}
+
+console.log(even(3))
+*/
+
---
+++ praticando/javascript/abril-25/2-if-else-elseif.js
@@ -0,0 +1,24 @@
+//1.Crie uma função que recebe dois números e retorna o maior deles.
+/*
+function greater(n, n2) {
+    if (n > n2) {
+        return n
+    } else {
+        return n2
+    }
+}
+
+console.log(greater(10,2))
+
+
+//2.Crie uma função que verifica se um número é par ou ímpar. A função deve retornar "par" ou "ímpar".
+function evenOdd(n) {
+    if (n % 2 === 0) {
+        return `O número ${n} é par`
+    } else {
+        return `O número ${n} é ímpar`
+    }
+}
+
+console.log(evenOdd(7))
+*/
